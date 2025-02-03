1b14ba8564180976c4c9ca481f499017179b6698
fredly11 <buechelewd@gmail.com>
2025-02-03 16:16:45 -0600

    Back up to NPS 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,24 @@
-import { getParkData } from "./parkService.mjs";
-
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 const parkData = getParkData();
 
-
-function parkInfoTemplate(info) {
-    return `<a href="/" class="hero-banner__title">${info.name}</a>
-    <p class="hero-banner__subtitle">
-      <span>${info.designation}</span>
-      <span>${info.states}</span>
-    </p>`;
-  }
-
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
-document.querySelector("head > title").textContent = parkData.fullName;
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
-document.querySelector(".hero-banner__content").innerHTML =
-  parkInfoTemplate(parkData);
+function setParkIntro(data) {
+  const intro = document.querySelector(".intro");
+  intro.innerHTML = `<h1>${parkData.fullName}</h1>
+  <p>${parkData.description}</p>`;
+}
 
 
+function setParkInfoLinks(data) {
+  const info = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  info.insertAdjacentHTML("afterbegin", html.join(""));
+}
+
+
+
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
 
